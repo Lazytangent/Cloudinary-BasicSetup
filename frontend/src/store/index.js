2454a6ca13041949a8cbfc87e864b7f447c7412f
Lazytangent <pm123633@gmail.com>
2021-04-27 20:43:13 -0500

    Create component form and store stuff

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import sessionReducer from './session';
+import imagesReducer from './images';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  images: imagesReducer,
 });
 
 let enhancer;
